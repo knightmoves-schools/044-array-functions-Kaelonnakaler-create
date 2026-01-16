23fe88e8f9c7f1f83ec8ac9ef3b9f32294889952
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-01-16 14:23:42 -0600

    fixed automobile copy
    Fixed variable name and updated string interpolation.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let automobiles= ['car', 'truck', 'motorcycle'];
+let automobiles= ['car', 'truck', 'motorcycle']
 
 automoblies.push('airplane','skateboard');
 
 automobile.unshift('bike','helicopter');
 
-let automobilesCopy= 'automobiles';
+let automobilesCopy= `${automobiles}`;
 
 automobiles.pop();
 
 automoblies.shift();
 
 document.getElementById("result").innerHTML = automobiles;
+
 
